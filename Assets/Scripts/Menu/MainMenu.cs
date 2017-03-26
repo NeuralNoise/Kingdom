a42955cba0103727e3bcbd22cb375d636281c7f8
DESKTOP-DF2FGDR\Adrien <adrien.gannerie@gmail.com>
2017-03-27 00:36:05 +0200

    Ebauche du menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Solo()
+    {
+        SceneManager.LoadScene("Solo Menu");
+    }
+
+    public void Multi()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menu/SoloMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SoloMenu : MonoBehaviour
+{
+    public void Campaign()
+    {
+
+    }
+
+    public void Cancel()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
